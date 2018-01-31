254e9d8cc334ff116578d412819184b0fe123407
alch <epsilon_chi@outlook.com>
2018-01-31 13:45:30 -0800

    Screen boundry detection

--- Assets/DepthHandler.cs
+++ Assets/DepthHandler.cs
@@ -7,9 +7,6 @@ public class DepthHandler : MonoBehaviour {
 
 	// For every 1 unit in the y direction, objects with depth will scale down by this percent
 	private const float DEPTH_TO_SCALE_PERCENT = 0.2f;
-	// Minimum scale of things so they don't get too small
-	public float MIN_SIZE = 0.8f;
-	// 
 
 	// The y-coordinate at which this transform will shrink to scale = MIN_SIZE
 	public float Horizon {
@@ -25,12 +22,10 @@ public class DepthHandler : MonoBehaviour {
 		// As y gets bigger, sprite becomes smaller
 		// At y = 0, the transform has scale = 1
 		float scaleFactor = 1 - (transform.position.y * DEPTH_TO_SCALE_PERCENT);
-		scaleFactor = (scaleFactor > MIN_SIZE ? scaleFactor : MIN_SIZE);
 		transform.localScale = originalScale * scaleFactor;
 		// Debug
 		if (transform.position.y >= Horizon) {
 			Debug.Log ("Horizon reached!");
 		}
-		Debug.Log (transform.position.y);
 	}
 }
