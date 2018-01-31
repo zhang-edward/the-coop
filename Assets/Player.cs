a68c1b18e0c4421878a820413c1bc068eb954c84
alch <epsilon_chi@outlook.com>
2018-01-30 23:33:27 -0800

    Implemented artifical "seeable" horizone

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -17,8 +17,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float vx = Input.GetAxisRaw("Horizontal") * SPEED_X * ((transform.position.y - depth.Horizon) * -0.2f);
-		float vy = Input.GetAxisRaw("Vertical") * SPEED_Y * ((transform.position.y - depth.Horizon) * -0.2f);
+		float i = ((transform.position.y > 1 && Input.GetAxisRaw ("Vertical") == 1) ? 0f : 1f);
+		float vy = Input.GetAxisRaw("Vertical") * SPEED_Y * ((transform.position.y - depth.Horizon) * -0.2f) * i;
 
 		transform.Translate(vx, vy, 0);
+		Debug.Log (transform.position.x);
 	}
 }
